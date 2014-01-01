5c2f749f929660b2f4f5fd01f0a123c7214bca67
Matt Cordell <matt@stripemedia.com.au>
2014-01-02 10:12:27 +1100

    removed argument from init

--- index.js
+++ index.js
@@ -3,11 +3,7 @@ var util = require('util');
 var yeoman = require('yeoman-generator');
 
 var ChookGenerator = module.exports = function ChookGenerator(args, options, config) {
-  // By calling `NamedBase` here, we get the argument to the subgenerator call
-  // as `this.name`.
   yeoman.generators.NamedBase.apply(this, arguments);
-
-  console.log('You called the chook subgenerator with the argument ' + this.name + '.');
 };
 
 util.inherits(ChookGenerator, yeoman.generators.NamedBase);
